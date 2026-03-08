f3e22bec5b9af15dfd4e0c38365d0887d9210d5a
joon <hao.nguyenchanh2712@gmail.com>
2026-03-08 15:00:08 +0700

    Two Sum

---
+++ TwoSum/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace TwoSum
+{
+    public class Solution
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            Solution solution = new Solution();
+            solution.TwoSum(nums, target);
+        }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return new int[0];
+        }
+    }
+}
